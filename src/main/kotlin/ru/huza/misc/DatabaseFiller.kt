916c14b50b8a1f1471fbba122f43d38ab20e4617
Mikhail Gostev <mikhail.gostev@arrival.com>
2022-09-14 16:07:35 +0300

    add more init data

--- src/main/kotlin/ru/huza/misc/DatabaseFiller.kt
+++ src/main/kotlin/ru/huza/misc/DatabaseFiller.kt
@@ -26,14 +26,16 @@ class DatabaseFiller(
     @PostConstruct
     fun fillDatabase() {
         createUsers()
+        createAssetDefs()
+    }
 
-        // "SPELL" — заклинание
-        // "RESOURCE" — ресурс
-        // "RECRUIT" — рекрут
+    private fun createAssetDefs() {
+        var assetDefCtr = 100000L
 
+        // ===== BUILDINGS =====
         val tavern = assetDefService.findByCode("tavern") ?: assetDefService.save(
             AssetDefDto(
-                id = 1,
+                id = assetDefCtr++,
                 type = "BUILDING",
                 code = "tavern",
                 name = "Таверна",
@@ -47,7 +49,7 @@ class DatabaseFiller(
 
         val villageHall = assetDefService.findByCode("village-hall") ?: assetDefService.save(
             AssetDefDto(
-                id = 2,
+                id = assetDefCtr++,
                 type = "BUILDING",
                 code = "village-hall",
                 name = "Сельская управа",
@@ -61,7 +63,7 @@ class DatabaseFiller(
 
         val townHall = assetDefService.findByCode("town-hall") ?: assetDefService.save(
             AssetDefDto(
-                id = 3,
+                id = assetDefCtr++,
                 type = "BUILDING",
                 code = "town-hall",
                 name = "Ратуша",
@@ -75,7 +77,7 @@ class DatabaseFiller(
 
         val cityHall = assetDefService.findByCode("city-hall") ?: assetDefService.save(
             AssetDefDto(
-                id = 4,
+                id = assetDefCtr++,
                 type = "BUILDING",
                 code = "city-hall",
                 name = "Магистрат",
@@ -89,7 +91,7 @@ class DatabaseFiller(
 
         val capitol = assetDefService.findByCode("capitol") ?: assetDefService.save(
             AssetDefDto(
-                id = 5,
+                id = assetDefCtr++,
                 type = "BUILDING",
                 code = "capitol",
                 name = "Капитолий",
@@ -98,6 +100,77 @@ class DatabaseFiller(
                 img130Url = null,
                 img250Url = null,
                 imgOrigUrl = "https://static.wikia.nocookie.net/heroesofmightandmagic/images/1/17/Капитолий_-_ЗамокH3.png/revision/latest?cb=20170119074117&path-prefix=ru"
+            )
+        )
+
+        // ===== SPELLS =====
+        val armageddon = assetDefService.findByCode("armageddon") ?: assetDefService.save(
+            AssetDefDto(
+                id = assetDefCtr++,
+                type = "SPELL",
+                code = "armageddon",
+                name = "Армагедон",
+                description = "Armageddon is a 4th level spell in the School of Fire Magic. It damages all creatures on the battlefield, including the caster's own troops. However, a few creatures can resist the spell due to their spell immunities.",
+                img75Url = null,
+                img130Url = null,
+                img250Url = null,
+                imgOrigUrl = "https://heroes.thelazy.net/images/archive/6/63/20170316101406%21Armageddon.png"
+            )
+        )
+
+        // ===== RESOURCES =====
+        val gold = assetDefService.findByCode("gold") ?: assetDefService.save(
+            AssetDefDto(
+                id = assetDefCtr++,
+                type = "RESOURCE",
+                code = "gold",
+                name = "Золото",
+                description = "Золото (ориг. Gold) – важнейший ресурс, присутствующий во всех играх серии Герои Меча и Магии, а также валюта в серии Меч и Магия.",
+                img75Url = null,
+                img130Url = null,
+                img250Url = null,
+                imgOrigUrl = "https://static.wikia.nocookie.net/heroesofmightandmagic/images/9/9b/Золото_-_H3.jpg/revision/latest?cb=20160419123135&path-prefix=ru"
+            )
+        )
+        val wood = assetDefService.findByCode("wood") ?: assetDefService.save(
+            AssetDefDto(
+                id = assetDefCtr++,
+                type = "RESOURCE",
+                code = "wood",
+                name = "Древесина",
+                description = "Древесина (англ. Wood) – один из трёх главных ресурсов во всех играх серии Герои Меча и Магии, чаще всего используемых при строительстве (наряду с золотом и рудой). Основным источником добычи являются лесопилки, приносящие 2 ед. древесины в день.",
+                img75Url = null,
+                img130Url = null,
+                img250Url = null,
+                imgOrigUrl = "https://static.wikia.nocookie.net/heroesofmightandmagic/images/e/e3/Древесина_-_H5.jpg/revision/latest?cb=20170626161819&path-prefix=ru"
+            )
+        )
+        val mercury = assetDefService.findByCode("mercury") ?: assetDefService.save(
+            AssetDefDto(
+                id = assetDefCtr++,
+                type = "RESOURCE",
+                code = "mercury",
+                name = "Ртуть",
+                description = "Ртуть (англ. Mercury) - ценный ресурс в серии игр Герои Меча и Магии. Он весьма редок, необходим для постройки и совершенствования высокоуровневых зданий и Гильдий Магов. Также ртуть потребуется для найма некоторых мощных юнитов.",
+                img75Url = null,
+                img130Url = null,
+                img250Url = null,
+                imgOrigUrl = "https://s00.yaplakal.com/pics/pics_original/9/5/1/4377159.jpg"
+            )
+        )
+
+        // ===== RECRUITS =====
+        val archangel = assetDefService.findByCode("archangel") ?: assetDefService.save(
+            AssetDefDto(
+                id = assetDefCtr++,
+                type = "RECRUIT",
+                code = "archangel",
+                name = "Архангел",
+                description = "Позволяет нанимать героев, а также повышает боевой дух войск гарнизона на 1 ед",
+                img75Url = null,
+                img130Url = null,
+                img250Url = null,
+                imgOrigUrl = "https://www.meme-arsenal.com/memes/f617aa0a967ad16f1c96627b6c66d59b.jpg"
             )
         )
 
@@ -111,6 +184,39 @@ class DatabaseFiller(
             AssetDto(
                 assetDef = villageHall.toLink(),
                 quantity = 1
+            )
+        )
+
+        assetService.save(
+            AssetDto(
+                assetDef = armageddon.toLink(),
+                quantity = 1
+            )
+        )
+
+        assetService.save(
+            AssetDto(
+                assetDef = gold.toLink(),
+                quantity = 1488
+            )
+        )
+        assetService.save(
+            AssetDto(
+                assetDef = wood.toLink(),
+                quantity = 32
+            )
+        )
+        assetService.save(
+            AssetDto(
+                assetDef = mercury.toLink(),
+                quantity = 3
+            )
+        )
+
+        assetService.save(
+            AssetDto(
+                assetDef = archangel.toLink(),
+                quantity = 10
             )
         )
     }
