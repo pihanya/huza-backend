1f9b2d398fb867551bc119a8aa4c8d119b6504b1
Mikhail Gostev <gostev.mike@gmail.com>
2023-02-15 17:26:28 +0300

    add assetDef avatar propagation into asset

--- huza-core/src/main/kotlin/ru/huza/core/model/dto/AssetDefLink.kt
+++ huza-core/src/main/kotlin/ru/huza/core/model/dto/AssetDefLink.kt
@@ -6,19 +6,22 @@ fun AssetDef.toLink(): AssetDefLink = AssetDefLink(
     id = this.id!!,
     type = this.type!!,
     code = this.code!!,
-    name = this.name!!
+    name = this.name!!,
+    imgOrigUrl = this.imgOrigUrl!!,
 )
 
 fun AssetDefDto.toLink(): AssetDefLink = AssetDefLink(
     id = this.id!!,
     type = this.type,
     code = this.code,
-    name = this.name
+    name = this.name,
+    imgOrigUrl = this.imgOrigUrl,
 )
 
 data class AssetDefLink(
     val id: Long,
     val type: String,
     val code: String,
-    val name: String
+    val name: String,
+    val imgOrigUrl: String,
 )
