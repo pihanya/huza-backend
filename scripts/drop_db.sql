f5875eb3360c3f481cb97e37b4861857fdf31836
Mikhail Gostev <gostev.mike@gmail.com>
2023-03-01 18:02:09 +0300

    add script for dropping db

---
+++ scripts/drop_db.sql
@@ -0,0 +1,17 @@
+DROP TABLE asset CASCADE;
+DROP TABLE asset_aud CASCADE;
+DROP SEQUENCE asset_seq;
+
+DROP TABLE asset_def CASCADE;
+DROP TABLE asset_def_aud CASCADE;
+DROP SEQUENCE asset_def_seq;
+
+DROP TABLE build_order CASCADE;
+DROP TABLE build_order_aud CASCADE;
+DROP SEQUENCE build_order_seq;
+
+DROP TABLE revinfo CASCADE;
+DROP SEQUENCE revinfo_seq;
+
+DROP TABLE service_user CASCADE;
+DROP SEQUENCE service_user_seq;
